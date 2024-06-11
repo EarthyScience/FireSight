19d09966034642389569d726974c6a6523441b9a
lalonso <lalonso@bgc-jena.mpg.de>
2024-06-11 23:41:17 +0100

    add inputs to folders

--- src/components/VolumeShader.tsx
+++ src/components/VolumeShader.tsx
@@ -35,10 +35,10 @@ const options3D = Vars_3D.map((element) => {
 
 
 import {
-  useListBlade,
+  // useListBlade,
   usePaneFolder,
-  // usePaneInput,
-  useSliderBlade,
+  usePaneInput,
+  // useSliderBlade,
   // useTextBlade,
   useTweakpane,
 } from '../../pane'
@@ -52,13 +52,20 @@ export function VolumeShader() {
   const pane = useTweakpane(
     {
       threshold: 0.0,
+      cmap: 'blackbody',
+      vName3d: 'cams_co2fire',
+      vName2d: 'area',
+      vName1d: 'fcci_ba_valid_mask',
     },
     {
-      title: 'Geometry Settings',
       container: containerElement,
     }
   )
-  const [threshold] = useSliderBlade(pane, {
+  const folderGeo = usePaneFolder(pane, {
+    title: 'Geometry Settings',
+  })
+
+  const [threshold] = usePaneInput(folderGeo, 'threshold', {
     label: 'threshold',
     value: 0.0,
     min: 0,
@@ -68,7 +75,7 @@ export function VolumeShader() {
   })
   // List blade
 // const cmap_texture = createTexture('blackbody')
-const [cmap_texture_name] = useListBlade(pane, {
+const [cmap_texture_name] = usePaneInput(folderGeo, 'cmap', {
   label: 'colormap',
   options: [
     {
@@ -91,24 +98,25 @@ const [cmap_texture_name] = useListBlade(pane, {
   value: 'blackbody'
 })
 
-// const folder = usePaneFolder(pane, {
-//   title: 'Variables',
-// })
 const cmap_texture =  createTexture(cmap_texture_name)
 
-const [drei_var] = useListBlade(pane, {
+const folderVars = usePaneFolder(pane, {
+  title: 'Variables',
+})
+
+const [drei_var] = usePaneInput(folderVars, 'vName3d', {
   label: '3D',
   options: options3D,
   value: 'test1'
 })
 
-const [twod_var] = useListBlade(pane, {
+const [twod_var] = usePaneInput(folderVars, 'vName2d', {
   label: '2D',
   options: options2D,
   value: 'test1'
 })
 
-const [one_var] = useListBlade(pane, {
+const [one_var] = usePaneInput(folderVars, 'vName1d', {
   label: '1D',
   options: options1D,
   value: 'test1'
