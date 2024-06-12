df2d6989d25e7506e13056787bed427cb36d2431
Lazaro Alonso <lazarus.alon@gmail.com>
2024-06-12 11:51:50 +0200

    better

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Navbar, Footer, CanvasGeometry, } from './components';
 import Overlays from "./components/Overlays";
 import { motion } from 'framer-motion';
 import { useState } from 'react';
+
 import './App.css'
 export function App() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const handleDrag = (event, info) => {
-    setPosition({ x: position.x + info.delta.x, y: position.y + info.delta.y });
+    setPosition({ x: info.point.x, y: info.point.y });
   };
   return (
     <>
@@ -16,13 +17,14 @@ export function App() {
     <Footer />
     <CanvasGeometry />
     <motion.div
-      id="myPane"
-      className='pane'
-      style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
-    >
-      <motion.div
-          className='drag-handle'
-          drag
+        id="myPane"
+        className='pane'
+        style={{ x: position.x, y: position.y }}
+      >
+        {/* Top left handle */}
+        <motion.div
+          className='drag-handle top-left'
+          drag='both' // 'both' is not allowed? but it does work???
           onDrag={handleDrag}
         />
     </motion.div>
