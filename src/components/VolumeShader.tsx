59aaa75dfa5d09ef40ec58fe42a71c3dfb9d00c9
Lazaro Alonso <lazarus.alon@gmail.com>
2024-08-12 13:03:21 +0200

    fixes volumeData types

--- src/components/VolumeShader.tsx
+++ src/components/VolumeShader.tsx
@@ -1,25 +1,29 @@
 import * as THREE from 'three';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useMemo } from 'react';
+import { Mesh, ShaderMaterial } from 'three';
 import { useFrame } from '@react-three/fiber';
-import vertexShader from '../utils/shaders/vertex.glsl'
-import fragmentShader from '../utils/shaders/fragment.glsl'
+import vertexShader from '../utils/shaders/vertex.glsl';
+import fragmentShader from '../utils/shaders/fragment.glsl';
 import ZarrLoaderLRU from './ZarrLoaderLRU';
-import { genRand} from '../utils/colormap'
+import { genRand } from '../utils/colormap';
 import { newVarData } from '../utils/volTexture';
 import { updateMetadataDescription } from '../utils/metadata';
 import { updateColorbar, getColors, rgbToHex } from '../utils/updateColorbar';
 import { useControlPane } from './PaneControls';
 
+type CustomMesh = Mesh & {
+  material: ShaderMaterial;
+};
+
 export function VolumeShader() {
+  const [meta, setMeta] = useState({});
+  const [volumeData, setVolumeData] = useState(() => genRand(50));
+  const [volumeText, setVolumeText] = useState<THREE.Data3DTexture | null>(null);
+  const [volumeShape, setVolumeShape] = useState(new THREE.Vector3(1, 1, 1));
+  const [minmax, setMinMax] = useState<[number, number]>([0.0, 1.0]);
+  const meshRef = useRef<CustomMesh>(null);
 
-  const [meta, setMeta] = useState({})
-  const [volumeText, setVolumeText] = useState()
-  const [volumeData, setVolumeData] = useState(null)
-  const [volumeShape, setVolumeShape] = useState(new THREE.Vector3(1,1,1))
-  const [minmax, setMinMax] =  useState<[number, number]>([0.0, 1.0]);
-  const meshRef = useRef()
-
-  const containerId = 'myPanePlugin'
+  const containerId = 'myPanePlugin';
   const {
     threshold,
     cmap_texture_name,
@@ -31,10 +35,9 @@ export function VolumeShader() {
     latmax,
     latmin,
     tmax,
-    tmin
+    tmin,
   } = useControlPane(containerId);
 
-  // do updates!
   useEffect(() => {
     updateColorbar({
       colorbarId: 'colorbar',
@@ -42,78 +45,84 @@ export function VolumeShader() {
       minmax,
       cmapTextureName: cmap_texture_name,
       getColors,
-      rgbToHex
+      rgbToHex,
     });
   }, [minmax, cmap_texture_name]);
-  
-  // This effect runs whenever meta changes
+
   useEffect(() => {
     return updateMetadataDescription(meta, 'myDescription');
   }, [meta]);
 
   useFrame(({ camera }) => {
-    meshRef.current.material.uniforms.cameraPos.value.copy(camera.position)
-  })
+    if (meshRef.current) {
+      const material = meshRef.current.material;
+      material.uniforms.cameraPos.value.copy(camera.position);
+    }
+  });
 
   useEffect(() => {
     if (!volumeData) {
       const randomArray = genRand(50);
       setVolumeData(randomArray);
-      return ;
+      return;
     }
-    
-    const [newText, newShape, minmax] = newVarData(volumeData)
-    setMinMax(minmax)
-    setVolumeText(newText)
 
-    // console.log('shape length:', volumeData.shape);
+    const [newText, newShape, newMinmax] = newVarData(volumeData);
+    setMinMax(newMinmax);
+    setVolumeText(newText);
 
     if (volumeData.shape.length === 3) {
-      setVolumeShape(new THREE.Vector3(2, newShape[1]/newShape[2]*2, 2)) //Dims are Z,Y,X
+      setVolumeShape(new THREE.Vector3(2, newShape[1] / newShape[2] * 2, 2)); //Dims are Z,Y,X
     } else if (volumeData.shape.length === 2) {
-      setVolumeShape(new THREE.Vector3(2, newShape[1]/newShape[2]*2, 0.05)) //Dims are Z,Y,X
+      setVolumeShape(new THREE.Vector3(2, newShape[1] / newShape[2] * 2, 0.05)); //Dims are Z,Y,X
     } else if (volumeData.shape.length === 1) {
-      setVolumeShape(new THREE.Vector3(2, newShape[1]/newShape[2]*2, 0.05)) //Dims are Z,Y,X, something is off here
+      setVolumeShape(new THREE.Vector3(2, newShape[1] / newShape[2] * 2, 0.05)); //Dims are Z,Y,X, something is off here
     } else {
       console.error('Unsupported shape length:', volumeData.shape.length);
     }
-  }, [volumeData])
+  }, [volumeData]);
+
+  const shaderMaterial = useMemo(() => ({
+    glslVersion: THREE.GLSL3,
+    uniforms: {
+      map: { value: volumeText },
+      cameraPos: { value: new THREE.Vector3() },
+      threshold: { value: threshold },
+      steps: { value: 200 },
+      scale: { value: volumeShape },
+      flip: { value: false },
+      cmap: { value: cmap_texture },
+      flatBounds: { value: new THREE.Vector4(lonmin, lonmax, tmin, tmax) },
+      vertBounds: { value: new THREE.Vector2(latmin, latmax) },
+    },
+    vertexShader,
+    fragmentShader,
+    side: THREE.BackSide,
+  }), [volumeText, threshold, volumeShape, cmap_texture, lonmin, lonmax, latmin, latmax, tmin, tmax]);
 
   return (
-  <>
-  <group position={[0,1.01,0]}>
-
-  <ZarrLoaderLRU variable={drei_var} setData={setVolumeData} slice={tInterval} setMeta={setMeta}/>
-  
-  <mesh ref={meshRef} rotation-y={Math.PI}>
-    <boxGeometry args={[2, 2, 2]} />
-    <shaderMaterial
-      attach="material"
-      args={[{
-        glslVersion: THREE.GLSL3,
-        uniforms: {
-          map: { value: volumeText },
-          cameraPos: { value: new THREE.Vector3() },
-          threshold: { value: threshold },
-          steps: { value: 200 },
-          scale: {value: volumeShape},
-          flip: {value: false},
-          cmap: {value: cmap_texture},
-          flatBounds: {value: new THREE.Vector4(lonmin, lonmax, tmin, tmax)},
-          vertBounds: {value: new THREE.Vector2(latmin, latmax)}
-        },
-        vertexShader,
-        fragmentShader,
-        side: THREE.BackSide,
-      }]}
-    />
-  </mesh>
-  <mesh castShadow>
-    <boxGeometry args={[volumeShape.x, volumeShape.y, volumeShape.z]} />
-    <meshStandardMaterial transparent color={'red'} visible={false} />
-  </mesh>
-  
-  </group>
-  </>
-  )
+    <>
+      <group position={[0, 1.01, 0]}>
+        <ZarrLoaderLRU
+          variable={drei_var}
+          setData={setVolumeData}
+          slice={tInterval}
+          setMeta={setMeta}
+        />
+        {/* {volumeText && ( activating this one fixes rendering on load, but introduces rendering artifacts*/}
+          <mesh ref={meshRef} rotation-y={Math.PI}>
+            <boxGeometry args={[2, 2, 2]} />
+            <shaderMaterial
+              attach="material"
+              args={[shaderMaterial]}
+            />
+          </mesh>
+      
+        <mesh castShadow>
+          <boxGeometry args={[volumeShape.x, volumeShape.y, volumeShape.z]} />
+          <meshStandardMaterial transparent color={'red'} visible={false} />
+        </mesh>
+      </group>
+    </>
+  );
 }
