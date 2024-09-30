2bbd8332ac8e60a850161ce7b426329a48526602
Lazaro <lazarus.alon@gmail.com>
2024-09-30 16:33:00 +0200

    fix r

--- src/components/AnalysisFunctions.tsx
+++ src/components/AnalysisFunctions.tsx
@@ -70,12 +70,12 @@ interface AnalyzerProps {
   }
 
 export const Analyzer = ({variable1, variable2, slice, setData}: AnalyzerProps) =>{
-    // console.log(variable1)
-    // console.log(variable2)
+    console.log(variable1)
+    console.log(variable2)
     const [data1, setData1] = useState()
     const [data2, setData2] = useState()
-    ZarrLoaderAnalysis({variable:variable1, setDataA:setData1, slice:{min:0, max:24}})
-    ZarrLoaderAnalysis({variable:variable2, setDataA:setData2, slice:{min:0, max:24}})
+    ZarrLoaderAnalysis({variable:variable1, setData:setData1, slice:{min:0, max:24}})
+    ZarrLoaderAnalysis({variable:variable2, setData:setData2, slice:{min:0, max:24}})
 
     useEffect(()=>{
         if (!data1|| !data2){
