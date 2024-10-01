cb2988217b1e53c580a86bc84f458af08b9813ab
Lazaro <lazarus.alon@gmail.com>
2024-10-01 11:19:35 +0200

    max logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,11 @@
 import logoSeasFire from "./logoS.png";
 import logoSF from "./SeasFire-logo.png";
 import logo from "./logo.png";
+import logoMPI from "./logo_mpi.png"
 
 export {
   logoSeasFire,
   logo,
-  logoSF
+  logoSF,
+  logoMPI
 };
