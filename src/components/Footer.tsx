70fe8e87c1c72ef01fda541eaeeab286efe73a4b
Lazaro Alonso <lazarus.alon@gmail.com>
2024-10-08 23:30:03 +0200

    better layout and logo links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ import './Footer.css';
 
 const Footer = () => (
   <div className="footer">
-    <img src={logoMPI} alt="logoMPI" height={35}/>
+    <a href="https://www.bgc-jena.mpg.de/en/bgi/mdi" target="_blank">
+      <img src={logoMPI} alt="logoMPI" height={35}/>
+    </a>
     <p>Copyright Ⓒ 2024. All Rights Reserved.</p>
     <span>Contact :<a href="https://lazarusa.github.io/" target='_blank'>Lazaro Alonso</a> </span>
   </div>
