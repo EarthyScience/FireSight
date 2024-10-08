8a9ebcf7e95f76e5e9fe7c0d6dbfa450c612d15b
Lazaro Alonso <lazarus.alon@gmail.com>
2024-10-09 00:55:52 +0200

    do a better footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,28 @@
-import { logoMPI } from "../assets/index";
+import { logoBGC_MPI, logoBGC, logoMPI } from "../assets/index";
 
 import './Footer.css';
 
 const Footer = () => (
   <div className="footer">
-    <a href="https://www.bgc-jena.mpg.de/en/bgi/mdi" target="_blank">
-      <img src={logoMPI} alt="logoMPI" height={35}/>
-    </a>
-    <p>Copyright Ⓒ 2024. All Rights Reserved.</p>
-    <span>Contact :<a href="https://lazarusa.github.io/" target='_blank'>Lazaro Alonso</a> </span>
+    <div className="large-screen-logo">
+      <a href="https://www.bgc-jena.mpg.de/en/bgi/mdi" target="_blank">
+        <img src={logoBGC_MPI} alt="logoMPI" height={35}/>
+      </a>
+    </div>
+    <div className="small-screen-logos">
+      <a href="https://www.bgc-jena.mpg.de/en/bgi/mdi" target="_blank">
+        <img src={logoBGC} alt="logoBGC" height={35}/>
+      </a>
+    </div>
+    <div className="expandable-text">
+      <p>Copyright Ⓒ 2024. All Rights Reserved.</p>
+      <span>Contact :<a href="https://lazarusa.github.io/" target='_blank'>Lazaro Alonso</a> </span>
+    </div>
+    <div className="small-screen-logos">
+      <a href="https://www.bgc-jena.mpg.de/en/bgi/home" target="_blank">
+        <img src={logoMPI} alt="logoMPI" className="small-screen-logo" height={35}/>
+      </a>
+    </div>
   </div>
 );
 
