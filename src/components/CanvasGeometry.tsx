a43be449dcec1629aabf48dfeae7597fc1acf5a8
Lazaro Alonso <lazarus.alon@gmail.com>
2024-10-09 22:52:27 +0200

    cleaner box, plus text labels

--- src/components/CanvasGeometry.tsx
+++ src/components/CanvasGeometry.tsx
@@ -17,7 +17,7 @@ import './CanvasGeometry.css'
 export function CanvasGeometry() {
   return (
     <div className='canvas'>
-      <Canvas shadows camera={{ position: [-4, 5, 4.5], fov: 50 }}>
+      <Canvas shadows camera={{ position: [-4, 3, 4.5], fov: 50 }}>
         {/* <Perf position="bottom-left" /> */}
           <FrameBoxed
             width={2.1} 
@@ -27,14 +27,16 @@ export function CanvasGeometry() {
           />
           <VolumeShader />
           {/* <ParticleInstances /> */}
+          
           <OrbitControls 
             enableDamping={true} 
             enablePan={false} 
             enableZoom={true} 
             minPolarAngle={0} 
-            maxPolarAngle={Math.PI / 2}
+            maxPolarAngle={Math.PI}
             target={new THREE.Vector3(0,1,0)}
-            />
+          />
+
           <ambientLight intensity={0.5} />
           <AccumulativeShadows temporal frames={200} color="black" colorBlend={0.5} opacity={0.5} scale={10} alphaTest={0.85}>
             <RandomizedLight amount={8} radius={5} ambient={0.5} position={[5, 3, 2]} bias={0.001} />
--- src/components/FrameBoxed.tsx
+++ src/components/FrameBoxed.tsx
@@ -1,13 +1,48 @@
 import * as THREE from 'three';
-export function FrameBoxed({ width = 2.2, height = 2.2, depth = 2.2, origin = [0, 1, 0] }) {
+import { Text } from '@react-three/drei';
+export function FrameBoxed({ 
+  width = 2.2,
+  height = 2.2,
+  depth = 2.2,
+  origin = [0, 1, 0]
+}) {
   const boxGeometry = new THREE.BoxGeometry(width, height, depth);
   const edges = new THREE.EdgesGeometry(boxGeometry); // Extract only the edges (no diagonals)
 
   return (
-    <mesh position={new THREE.Vector3(...origin)}>
+  <group position={new THREE.Vector3(...origin)}>
+    <mesh>
       <lineSegments geometry={edges}>
         <lineBasicMaterial color="#c8cdd2" />
       </lineSegments>
     </mesh>
-  );
+
+    <Text
+      position={[0, -height / 2, depth / 1.8]} // Right-Front of the box
+      fontSize={0.1}
+      color="#c8cdd2"
+      // rotation={[-Math.PI / 2, 0, 0]}
+    >
+      depth / time
+    </Text>
+
+    <Text
+      position={[-width / 1.8, -height / 2, 0]} // Front edge of the box
+      fontSize={0.1}
+      color="#c8cdd2"
+      rotation={[0, -Math.PI / 2, 0]}
+    >
+      x / lon / longitude
+    </Text>
+
+    <Text
+      position={[-width / 1.8, 0, -depth / 2]} // Vertical-left-front edge
+      fontSize={0.1}
+      color="#c8cdd2"
+      rotation={[0, 0, Math.PI / 2]}
+    >
+      y / lat / latitude
+    </Text>
+</group>
+);
 }
